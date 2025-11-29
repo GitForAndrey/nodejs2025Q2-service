f6e4054765371301f5ca11b6fd823b2b82f288ad
andrii <andrii@gmail.com>
2025-11-29 22:44:00 +0300

    feat: add albums module

---
+++ src/Favorites/album.service.ts
@@ -0,0 +1,56 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { randomUUID } from 'crypto';
+import { AlbumEntity } from './entities/album.entity';
+import { Album } from './interfaces/album.interfase';
+import { CreateAlbumDto } from './dto/create-album.dto';
+import { UpdateAlbumDto } from './dto/update-album.dto';
+
+@Injectable()
+export class AlbumsService {
+    private albums: AlbumEntity[] = [];
+
+  create(dto: CreateAlbumDto): Album {
+    
+    const album: AlbumEntity = {
+      id: randomUUID(),
+      name: dto.name,
+      year: dto.year,
+      artistId: dto.artistId,
+      
+    };
+    this.albums.push(album);
+    return album;
+  }
+
+  findAll(): Album[] {
+  return this.albums;
+}
+
+  findOne(id: string): Album {
+    const album = this.albums.find((u) => u.id === id);
+    if (!album) {
+      throw new NotFoundException('Artist not found');
+    }
+
+    return album;
+  }
+
+  update(id: string, dto: UpdateAlbumDto): Album {
+
+    const album = this.albums.find((u) => u.id === id);
+    if (!album) {
+      throw new NotFoundException('Album not found');
+    }
+
+    Object.assign(album, dto);
+    return album;
+  }
+
+  remove(id: string): void {
+    const before = this.albums.length;
+    this.albums = this.albums.filter((u) => u.id !== id);
+    if (this.albums.length === before) {
+      throw new NotFoundException('Album not found');
+    }
+  }
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './User/user.module';
 import { TracksModule } from './Track/track.module';
 import { ArtistsModule } from './Artist/artist.module';
+import { AlbumsModule } from './Album/album.module';
 
 @Module({
-  imports: [UsersModule, TracksModule, ArtistsModule],
+  imports: [UsersModule, TracksModule, ArtistsModule, AlbumsModule],
 })
 export class AppModule {}
